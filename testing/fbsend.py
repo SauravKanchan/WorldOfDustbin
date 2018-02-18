6e7aa44f6140fdc952d7adc2d9ba975110d8b27e
Saurav Kanchan <sauravnk30@gmail.com>
2018-02-18 10:59:28 +0530

    .

--- testing/fbsend.py
+++ testing/fbsend.py
@@ -2,7 +2,7 @@ from firebase import firebase
 import json
 
 firebase = firebase.FirebaseApplication('https://dj-hack.firebaseio.com', None)
-result = firebase.get('/dustbins/', None)
+# result = firebase.get('/dustbins/', None)
 #print(result)
 data = {'cash': 50, 'currentUser': '', 'parent': '', 'percentFull': 22, 'queryText': 'Daddy 2 wants to buy a dustbin from Daddy 1', 'queryType': 0, 'streak': 0}
 data2 = {'cash': 50, 'currentUser': '', 'parent': '', 'percentFull': 82, 'queryText': 'Daddy 1 agrees to sell a dustbin to Daddy 2', 'queryType': 1, 'streak': 50}
--- testing/scene2.py
+++ testing/scene2.py
@@ -4,22 +4,20 @@ import time
 from firebase import firebase
 
 firebase = firebase.FirebaseApplication('https://dj-hack.firebaseio.com', None)
-result = firebase.get('/dustbins/', None)
+# result = firebase.get('/dustbins/', None)
 
 GPIO.setmode(GPIO.BCM)
 
 TRIG = 23
 ECHO = 24
 
-print
-"Distance Measurement In Progress"
+print "Distance Measurement In Progress"
 
 GPIO.setup(TRIG, GPIO.OUT)
 GPIO.setup(ECHO, GPIO.IN)
 
 GPIO.output(TRIG, False)
-print
-"Waiting For Sensor To Settle"
+print "Waiting For Sensor To Settle"
 time.sleep(2)
 
 current_distance = 0
@@ -45,13 +43,20 @@ while (c):
     pd = round(100 - distance * 100 / 31, 2)
 
     print "Percentage:", pd, "%"
-    data3 = {'cash': 300, 'currentUser': '', 'parent': '', 'percentFull': pd, 'queryText': '',
-             'queryType': -1, 'streak': 0}
+
+
+    #data3 => daddy1
+    data3 = {'cash': 300, 'currentUser': '', 'parent': '', 'percentFull': pd, 'queryText': '','queryType': -1, 'streak': 0}
+    data4 = {'cash': 300, 'currentUser': '', 'parent': '', 'percentFull': 0, 'queryText': '','queryType': -1, 'streak': 0}
     if (pd >= 80.0 ):
-        data3 = {'cash': 300, 'currentUser': '', 'parent': '', 'percentFull': pd,
-                 'queryText': 'Daddy1 wants to buy dustbin from amazon',
+        data3 = {'cash': 270, 'currentUser': '', 'parent': '', 'percentFull': 100,
+                 'queryText': 'Daddy1 wants to buy dustbin from Daddy2',
+                 'queryType': 2, 'streak': 5}
+        data4 = {'cash': 330, 'currentUser': '', 'parent': '', 'percentFull': 0,
+                 'queryText': 'Daddy2 agrees to sell dustbin Daddy1',
                  'queryType': 2, 'streak': 0}
 
     firebase.put('/dustbins', "1518880087182", data3)
+    firebase.put('/dustbins',"1518880134851",data4)
     c -= 1
 GPIO.cleanup()
